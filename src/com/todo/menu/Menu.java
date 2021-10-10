0da3c7e0cf0ce071c9e1054b57e8c79e26bb6fee
finess2971 <21800264@handong.edu>
2021-10-10 23:55:57 +0900

    Modify

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -9,7 +9,7 @@ public class Menu {
         System.out.println("2. 리스트 항목 삭제 (del)");
         System.out.println("3. 리스트 항목 수정 (edit)");
         System.out.println("4. 리스트 전체 목록 (ls)");
-        System.out.println("5. 리스트 제목순 정렬 (ls_name_asc)");
+        System.out.println("5. 리스트 제목순 정렬 (ls_name)");
         System.out.println("6. 리스트 제목역순 정렬 (ls_name_desc)");
         System.out.println("7. 리스트 생성순 정렬 (ls_date)");
         System.out.println("8. 리스트 생성역순 정렬 (ls_date_desc)");
